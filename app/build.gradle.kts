581dbe04fa7def16a1faf8245b57b1918b49e28a
ravinduranganaRv <ranganaravindu200007@gmail.com>
2025-08-28 02:31:01 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
 
 android {
